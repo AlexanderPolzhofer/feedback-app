7a5e09128eb253b503482fa92b7397f2e0906b81
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2023-03-11 21:40:42 +0100

    init project

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import FeedbackList from "./components/FeedbackList.svelte";
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "This is the feedback text für the highest possible rating.",
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: "Irure in nisi dolor voluptate magna Lorem aliquip esse est nulla aute do.",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "Irure in nisi dolor voluptate magna Lorem aliquip esse est nulla aute do.",
+    },
+  ];
+
+  const deleteFeedback = (e) => {
+    const itemId = e.detail;
+    feedback = feedback.filter((item) => item.id !== itemId);
+  };
+</script>
+
+<main class="container">
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+</main>
