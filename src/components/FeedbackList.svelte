661e6b4b95516e033a34380a442ab2d128aebad6
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2023-03-15 21:38:19 +0100

    add transition to FeedbackItem onAdd and onDelete

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,9 +1,11 @@
 <script>
+  import { fade, scale } from "svelte/transition";
   import FeedbackItem from "./FeedbackItem.svelte";
   export let feedback = [];
 </script>
 
-<h1>FeedbackList</h1>
 {#each feedback as fb (fb.id)}
-  <FeedbackItem item={fb} on:delete-feedback />
+  <div in:scale out:fade={{ duration: 500 }}>
+    <FeedbackItem item={fb} on:delete-feedback />
+  </div>
 {/each}
